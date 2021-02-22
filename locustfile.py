c48ec38ca7d990e254c3e23d6512ce112d427062
Valkea <github-sub@shedge.com>
2021-02-22 23:22:02 +0100

    feat: add initial locust performance tests

---
+++ locustfile.py
@@ -0,0 +1,33 @@
+from locust import HttpUser, between, task
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(3, 10)
+
+    def on_start(self):
+
+        self.club = "Simply Lift"
+        self.competitions = ["Spring Festival", "Fall Classic"]
+
+        # Visit Home page
+        self.client.get("/")
+
+        # Login
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+
+    def on_stop(self):
+        self.client.get("/logout")
+
+    @task(2)
+    def book(self):
+        for competition in self.competitions:
+            self.client.get(f"/book/{competition}/{self.club}", name="/book")
+
+    @task(1)
+    def purchasePlaces(self):
+        for competition in self.competitions:
+            self.client.post(
+                "/purchasePlaces",
+                {"places": 1, "club": self.club, "competition": competition},
+                name="/purchasePlaces",
+            )
