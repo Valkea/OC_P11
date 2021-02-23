b5b9f4a0e1ba7e6adef200e48cc8cd438f716762
Valkea <github-sub@shedge.com>
2021-02-23 22:19:28 +0100

    refactor: Clubs can't book more than 12 places per competition anymore

--- server.py
+++ server.py
@@ -21,6 +21,30 @@ app.secret_key = "something_special"
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+booking = {}
+
+
+def addBooking(club, competition, places):
+    """ save club's booking to competitions in a dictionnay """
+
+    if club not in booking:
+        booking[club] = {}
+
+    if competition not in booking[club]:
+        booking[club][competition] = 0
+
+    booking[club][competition] += places
+
+
+def getBooking(club, competition):
+    """ return the current club's booking number for a given competition """
+
+    if club not in booking:
+        return 0
+    if competition not in booking[club]:
+        return 0
+
+    return booking[club][competition]
 
 
 @app.route("/")
@@ -60,17 +84,29 @@ def purchasePlaces():
 
     placesRequired = int(request.form["places"])
     club_points = int(club["points"])
+    place_cost = 1
 
-    if placesRequired <= club_points:
-        club["points"] = club_points - placesRequired
+    if club_points < placesRequired * place_cost:
+
+        flash("You don't have enough points available")
+        status_code = 400
+
+    elif placesRequired + getBooking(club["name"], competition["name"]) > 12:
+
+        flash("You can't book more than 12 places per competition")
+        status_code = 400
+
+    else:
+
+        club["points"] = club_points - (placesRequired * place_cost)
         competition["numberOfPlaces"] = (
             int(competition["numberOfPlaces"]) - placesRequired
         )
+
+        addBooking(club["name"], competition["name"], placesRequired)
+
         flash("Great-booking complete!")
         status_code = 200
-    else:
-        flash("You don't have enough points available")
-        status_code = 400
 
     return (
         render_template("welcome.html", club=club, competitions=competitions),
